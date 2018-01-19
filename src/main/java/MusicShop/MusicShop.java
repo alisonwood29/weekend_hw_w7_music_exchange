9025de2099034886f0e8e245d136b01cdd725650
Alison Wood <alisonwood29@gmail.com>
2018-01-19 13:41:03 +0000

    created music shop class

--- src/main/java/MusicShop/MusicShop.java
+++ src/main/java/MusicShop/MusicShop.java
@@ -1,4 +1,14 @@
 package MusicShop;
 
+import java.util.ArrayList;
+
 public class MusicShop {
+
+    private String name;
+    private ArrayList<ISellable> stock;
+
+    public MusicShop(String name){
+        this.name = name;
+        this.stock = new ArrayList<>();
+    }
 }
